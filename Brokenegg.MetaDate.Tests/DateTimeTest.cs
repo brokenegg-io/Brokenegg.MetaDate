1d5aa366af55144d65a47ce0ce45c3aeb495fa11
Marco Lima <marckdx@outlook.com>
2021-08-02 20:32:37 -0300

    added github ci and started project

---
+++ Brokenegg.MetaDate.Tests/DateTimeTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Brokenegg.MetaDate.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDateConversion()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ Brokenegg.MetaDate/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Brokenegg.MetaDate
+{
+    public class Class1
+    {
+    }
+}
